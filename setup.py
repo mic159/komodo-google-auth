658ac064ef268a6f5c2d95bd0a2c2094336e5690
Michael Cooper <mic159@gmail.com>
2016-06-15 17:04:21 +1000

    Adding info for pypi

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        'flask-login==0.3.2',
-        'python-social-auth==0.2.14',
-        'sqlalchemy==1.0.12',
-        'komodo',
-    ]
+        'flask-login<=1.0.0',
+        'python-social-auth<0.3.0',
+        'sqlalchemy>=1.0.0,<2.0.0',
+        'komodo>=1.0.0,<2.0.0',
+    ],
+
+    # metadata for upload to PyPI
+    url = "https://github.com/mic159/komodo-google-auth",
+    author = "Michael Cooper",
+    author_email = "mic159@gmail.com",
+    description = "Plugin for Komodo dashboard that enables authentication behind a google login",
+    license = "MIT",
+    keywords = "dashboard",
 )
